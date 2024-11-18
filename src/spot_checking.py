4d0cf63ff1a2580d987ead9da6637f0322340aed
PLLima <74931964+PLLima@users.noreply.github.com>
2024-11-17 22:37:26 -0300

    refactor: complete model training refactoring

--- src/spot_checking.py
+++ src/spot_checking.py
@@ -12,72 +12,54 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import KFold
 
-def generate_train_validation_sets(train_indexes, validation_indexes, X, X_mandatory, y, y_mandatory):
-    for train_index, validation_index in zip(train_indexes, validation_indexes):
-        X_train, X_val = X_train.iloc[train_index], X_train.iloc[validation_index]
-        y_train, y_val = y_train.iloc[train_index], y_train.iloc[validation_index]
-        # If no_electric_cars, join the forced test_case with the random test_case
-        # Concatanate the mandatory and training sets
-        X_train = pd.concat([X_mandatory, X_train])
-        y_train = pd.concat([y_mandatory, y_train])
+def generate_train_validation_sets(train_indexes, validation_indexes, X_train, X_mandatory, y_train, y_mandatory):
+    X_train, X_val = X_train.iloc[train_indexes], X_train.iloc[validation_indexes]
+    y_train, y_val = y_train.iloc[train_indexes], y_train.iloc[validation_indexes]
+    # If no_electric_cars, join the forced test_case with the random test_case
+    # Concatanate the mandatory and training sets
+    X_train = pd.concat([X_mandatory, X_train])
+    y_train = pd.concat([y_mandatory, y_train])
     return X_train, X_val, y_train, y_val
 
-def evaluate_model(kfolds, X_train, y_train, X_train_mandatory, y_train_mandatory, method):
-    random_state = 42
-    model_metrics = []  # List to store metrics for each split
+def evaluate_model(method, X_train, X_val, y_train, y_val, random_state):
+    # Preprocessing is sensitive to type
+    # Separate analyzed features into numerical and categorical
+    # As to apply preprocessing only to valid features
+    numerical_features = X_train.select_dtypes(include=['int64', 'float64']).columns
+    categorical_features = X_train.select_dtypes(include=['object', 'bool']).columns
 
-    # Separate validation data and the remaining instances
-    #X_train, X_val, y_train, y_val  = train_test_split(X, y, test_size=val_size)
-    # Split the remaining instances in kfolds parts
-    kf = KFold(n_splits=kfolds, shuffle=True, random_state=random_state)
-    for train_index, validation_index in kf.split(X_train):
-        X_train, X_val = X_train.iloc[train_index], X_train.iloc[validation_index]
-        y_train, y_val = y_train.iloc[train_index], y_train.iloc[validation_index]
-        # If no_electric_cars, join the forced test_case with the random test_case
-        # Concatanate the mandatory and training sets
-        X_train = pd.concat([X_train_mandatory, X_train])
-        y_train = pd.concat([y_train_mandatory, y_train])
+    # ColumnTransformer applies preprocessing patterns e.g. StandardScaler() and
+    # OneHotEncoder() to groups, e.g. numerical_features and categorical_features
+    # Preprocessor will be applied to every dataset
+    preprocessor = ColumnTransformer([
+        ("num", StandardScaler(), numerical_features),
+        ("cat", OneHotEncoder(), categorical_features)
+    ])
+    # Select model and configuration for use in the pipeline
+    match method:
+        case "knn":
+            model = KNeighborsClassifier()
+        case "random_forest":
+            model = RandomForestRegressor(random_state=random_state)
+        case "linear_regression":
+            model = LinearRegression()
+        case "neural_networks":
+            model = MLPRegressor(max_iter=2500, random_state=random_state)
+        case "svm":
+            model = SVR()
+    # Pipeline applies the preprocessed dataset to the model for fitting
+    pipe = Pipeline([
+        ("preprocessor", preprocessor),
+        ("regressor", model)
+    ])
 
-        # Preprocessing is sensitive to type
-        # Separate analyzed features into numerical and categorical
-        # As to apply preprocessing only to valid features
-        numerical_features = X_train.select_dtypes(include=['int64', 'float64']).columns
-        categorical_features = X_train.select_dtypes(include=['object', 'bool']).columns
+    # Fitting the pipeline
+    pipe.fit(X_train, y_train)
 
-        # ColumnTransformer applies preprocessing patterns e.g. StandardScaler() and
-        # OneHotEncoder() to groups, e.g. numerical_features and categorical_features
-        # Preprocessor will be applied to every dataset
-        preprocessor = ColumnTransformer([
-            ("num", StandardScaler(), numerical_features),
-            ("cat", OneHotEncoder(), categorical_features)
-        ])
-        # Select model and configuration for use in the pipeline
-        match method:
-            case "knn":
-                model = KNeighborsClassifier()
-            case "random_forest":
-                model = RandomForestRegressor(random_state=random_state)
-            case "linear_regression":
-                model = LinearRegression()
-            case "neural_networks":
-                model = MLPRegressor(max_iter=2500, random_state=random_state)
-            case "svm":
-                model = SVR()
-        # Pipeline applies the preprocessed dataset to the model for fitting
-        pipe = Pipeline([
-            ("preprocessor", preprocessor),
-            ("regressor", model)
-        ])
+    # Prediction and evaluation
+    model_prediction = pipe.predict(X_val)
 
-        # Fitting the pipeline
-        pipe.fit(X_train, y_train)
-
-        # Prediction and evaluation
-        y_pred = pipe.predict(X_val)
-
-        metric = mean_squared_error(y_val, y_pred)
-        model_metrics.append(metric)
-
-    return model_metrics, y_pred
+    metric = mean_squared_error(y_val, model_prediction)
+    
+    return metric, model_prediction
