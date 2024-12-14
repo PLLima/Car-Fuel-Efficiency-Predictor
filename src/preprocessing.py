f25e40e2b6134e47c830a2aa253804a6d59ab234
PLLima <74931964+PLLima@users.noreply.github.com>
2024-12-13 23:57:45 -0300

    fix: make filter_categories' threshold work with percentages

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -16,14 +16,15 @@ def remove_outliers(df, atributes):
         df = df[(df[atribute] >= lower_bound) & (df[atribute] <= upper_bound)]
     return df
 
-def filter_categories(df, categorical_features, threshold=10):
+def filter_categories(df, categorical_features, threshold=0.1):
 # Replace less common categories with 'others'
+    total_instances = len(df)
     for feature in categorical_features:
         # Count occurrences of each category
         category_counts = df[feature].value_counts()
         
-        # Identify categories that occur less than the threshold
-        rare_categories = category_counts[category_counts < threshold].index
+        # Identify categories that occur proportionally less than the threshold
+        rare_categories = category_counts[(category_counts/total_instances) < threshold].index
         
         # Replace rare categories with 'others'
         df[feature] = df[feature].apply(lambda x: 'others' if x in rare_categories else x)
