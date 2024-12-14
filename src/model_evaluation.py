19f79b5612f98c95bc70a8d9b6c9610465283325
PLLima <74931964+PLLima@users.noreply.github.com>
2024-12-14 16:59:18 -0300

    feat: implement optimize_hyperparameters function

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from sklearn.neighbors import KNeighborsRegressor
@@ -10,6 +11,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import mean_squared_error
 
@@ -61,3 +63,45 @@ def evaluate_model(method, X_train, X_val, y_train, y_val, random_state):
     model_prediction = pipe.predict(X_val)
 
     return mean_squared_error(y_val, model_prediction)
+
+def optimize_hyperparameters(method, X_train, X_val, y_train, y_val, kfolds, random_state):
+    # Preprocessing is sensitive to type
+    # Separate analyzed features into numerical and categorical
+    # As to apply preprocessing only to valid features
+    numerical_features = X_train.select_dtypes(include=['int64', 'float64']).columns
+    categorical_features = X_train.select_dtypes(include=['object', 'bool', 'string']).columns
+
+    # ColumnTransformer applies preprocessing patterns e.g. MinMaxScaler() and
+    # OneHotEncoder() to groups, e.g. numerical_features and categorical_features
+    # Preprocessor will be applied to every dataset
+    preprocessor = ColumnTransformer([
+        ("num", MinMaxScaler(), numerical_features),
+        ("cat", OneHotEncoder(handle_unknown='infrequent_if_exist'), categorical_features)
+    ])
+    # Select model and configuration for use in the pipeline
+    match method:
+        case "random_forest":
+            hyperparameters = {
+                "n_estimators": np.arange(50, 500, 50),
+                "cpp_alpha": np.arange(0, 0.75, 0.05),
+            }
+            model = RandomForestRegressor(random_state=random_state)
+        case "neural_networks":
+            hyperparameters = {
+                "hidden_layer_sizes": [(100,), (50, 100), (50, 50), (100, 100), (50, 100, 50)],
+            }
+            model = MLPRegressor(early_stopping=True, n_iter_no_change=5, max_iter=2500, random_state=random_state)
+    
+    # Pipeline applies the preprocessed dataset to the model for fitting
+    pipe = Pipeline([
+        ("preprocessor", preprocessor),
+        ("regressor", model)
+    ])
+
+    # Parameters optimization
+    clf = GridSearchCV(pipe, hyperparameters, scoring='neg_mean_squared_error', cv=kfolds, verbose=1)
+
+    # Fitting the pipeline
+    clf.fit(X_train, y_train)
+
+    return clf.best_params_, clf.best_score_
